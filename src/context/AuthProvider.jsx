11b57e975743a3d397c7a6540a88db04f1db812b
Abu Hanif <hanifcse90@gmail.com>
2024-06-13 15:30:23 +0600

    Google Firebae Filse Setup

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { app } from "../firebase/firebse.config";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  // Create Email use User Email And Password ..
+  const createUserWithEmail = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // sign in Use User Email and Password
+  const signInWithEmail = (auth, email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // Update User Profile
+  const updateUserProfile = (userInfo) => {
+    return updateProfile(auth.currentUser, userInfo);
+  };
+  // Google Login
+  const googleLogin = (provider) => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+
+  // Log Out
+  const logout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // User Observer
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+  const authInfo = {
+    user,
+    createUserWithEmail,
+    loading,
+    signInWithEmail,
+    updateUserProfile,
+    logout,
+    googleLogin,
+  };
+  return (
+    <div>
+      <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    </div>
+  );
+};
+
+export default AuthProvider;
---
+++ src/firebase/firebse.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByukyZLw8SqVkSUI2RpQtLMUNKjmWcQZk",
+  authDomain: "stridejobtask1.firebaseapp.com",
+  projectId: "stridejobtask1",
+  storageBucket: "stridejobtask1.appspot.com",
+  messagingSenderId: "261316401710",
+  appId: "1:261316401710:web:84de1a0a63063d191f5dbf",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
